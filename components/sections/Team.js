e9915e101768bf9e8f685290d6e1a82865a95e02
Xavier Vila <xavier.vila@trilogi.com>
2022-05-30 13:54:27 +0200

    Moved members data

--- components/sections/Team.js
+++ components/sections/Team.js
@@ -1,11 +1,30 @@
 import TeamCard from "components/TeamCard";
 
 export default function Team({ t }) {
+  const dataMembers = [
+    {
+      name: "Alejandro Moreno",
+      rol: t("team.cardAlejandro.rol"),
+      twitter: "#",
+      github: "#",
+      linkedin: "#",
+      text: t("team.cardAlejandro.text"),
+    },
+    {
+      name: "Prova",
+      rol: t("team.cardAlejandro.rol"),
+      twitter: "#",
+      github: "#",
+      linkedin: "#",
+      text: t("team.cardAlejandro.text"),
+    },
+  ];
+
   return (
     <section id="team">
       <h2 className="mb-12">{t("team.title")}</h2>
       <div className=" container items-center justify-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-12 sm:px-1">
-        <TeamCard
+        {/* <TeamCard
           name={"Alejandro Moreno"}
           rol={t("team.cardAlejandro.rol")}
           twitter={"#"}
@@ -20,7 +39,18 @@ export default function Team({ t }) {
           github={"#"}
           linkedin={"#"}
           text={t("team.cardAlejandro.text")}
-        />
+        /> */}
+        {dataMembers.map(({ name, rol, twitter, github, linkedin, text }) => (
+          <TeamCard
+            key={name}
+            name={name}
+            rol={rol}
+            twitter={twitter}
+            github={github}
+            linkedin={linkedin}
+            text={text}
+          />
+        ))}
       </div>
     </section>
   );
