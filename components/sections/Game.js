6ce2fdd7b38a9cbf9d106ca3cff5fb6e22d077bc
Denisdrk6 <denisdsuniversidad@hotmail.com>
2022-06-15 01:53:49 +0200

    fixed youtube link

--- components/sections/Game.js
+++ components/sections/Game.js
@@ -14,7 +14,7 @@ export default function Game({ t }) {
             <iframe
               width="560"
               height="315"
-              src="https://youtu.be/Qi9ypqdbYCM"
+              src="https://www.youtube.com/watch?v=Qi9ypqdbYCM"
               title="YouTube video player"
               className="w-full"
               frameBorder={"0"}
